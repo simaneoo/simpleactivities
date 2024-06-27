8c345695ea60e174e0e0b1f755ef48a7e1423d63
simaneoo <157126327+simaneoo@users.noreply.github.com>
2024-06-27 13:46:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sélectionne le bouton avec l'ID "myButton"
+const button = document.getElementById('myButton');
+
+// Ajoute un écouteur d'événement pour le clic sur le bouton
+button.addEventListener('click', function() {
+    // Affiche une alerte lorsqu'on clique sur le bouton
+    alert('Bonjour! Vous avez cliqué sur le bouton.');
+});
